9c2cfb24893e49f81e4394fd0d9378b1ce0d89ed
Benjamin Ting <tingzhb@gmail.com>
2022-07-05 00:16:23 +0200

    Made Anty Random

---
+++ SOCStoryGame 1/Assets/Scripts/Controller/AntRandomizer.cs
@@ -0,0 +1,10 @@
+using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class AntRandomizer : MonoBehaviour {
+	private void Awake(){
+		var randomX = Random.Range(-570, 500);
+		transform.Translate(randomX, 0, 0);
+	}
+}
