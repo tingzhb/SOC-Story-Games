0fd1accda89828424c2ebab9dd08d822fe7d552b
Benjamin Ting <tingzhb@gmail.com>
2022-07-08 11:29:27 +0200

    Refined Drag and drop verification

--- SOCStoryGame 1/Assets/Scripts/Tools And Effects/DropVerification.cs
+++ SOCStoryGame 1/Assets/Scripts/Tools And Effects/DropVerification.cs
@@ -4,18 +4,17 @@ using Random = UnityEngine.Random;
 
 public class DropVerification : MonoBehaviour{
 	[SerializeField] private GameObject[] droppedItems;
-	[SerializeField] private int boundsLimits;
-	private Bounds bounds;
+
 	private int totalDropped;
 	private Executor executor;
 	private void Awake(){
-		bounds = new Bounds(transform.position, new Vector3(boundsLimits, boundsLimits, boundsLimits));
+
 		executor = FindObjectOfType<Executor>();
 	}
 
 	public void Verify(){
 		foreach (var item in droppedItems){
-			if (bounds.Contains(item.transform.position)){
+			if (GetComponent<BoxCollider2D>().bounds.Contains(item.transform.position)){
 				totalDropped++;
 			}
 		}
