0aab8a3188984307b561b8a4a1a5525addc58624
Benjamin Ting <tingzhb@gmail.com>
2022-07-11 10:36:08 +0200

    Complete C7

---
+++ SOCStoryGame 1/Assets/Scripts/Tools And Effects/C7Chooser.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class C7Chooser : MonoBehaviour{
+	[SerializeField] private GameObject choice, crab, cow, cat;
+
+	public void ChooseCrab(){
+		choice.SetActive(true);
+		crab.SetActive(true);
+	}
+	public void ChooseCow(){
+		choice.SetActive(true);
+		cow.SetActive(true);
+	}
+	public void ChooseCat(){
+		choice.SetActive(true);
+		cat.SetActive(true);
+	}
+}
