19bbdf02b0a0f4865da2ce53057c6725e0e21d3d
Benjamin Ting <tingzhb@gmail.com>
2022-07-20 10:25:57 +0200

    Delay level loading

--- SOCStoryGame 1/Assets/Scripts/Model/Level.cs
+++ SOCStoryGame 1/Assets/Scripts/Model/Level.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -5,6 +6,11 @@ public class Level : MonoBehaviour{
 	[SerializeField] public string sceneName;
 
 	public void GoToLevel(){
+		StartCoroutine(DelayLoadLevel());
+	}
+
+	private IEnumerator DelayLoadLevel(){
+		yield return new WaitForSeconds(0.5f);
 		SceneManager.LoadSceneAsync(sceneName);
 	}
 }
