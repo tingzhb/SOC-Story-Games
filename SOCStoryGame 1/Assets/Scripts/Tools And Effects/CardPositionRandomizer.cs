57acfd151e60b6bf4515122632495f5a30328056
Benjamin Ting <tingzhb@gmail.com>
2022-07-25 11:56:32 +0200

    Edit Build order

--- SOCStoryGame 1/Assets/Scripts/Tools And Effects/CardPositionRandomizer.cs
+++ SOCStoryGame 1/Assets/Scripts/Tools And Effects/CardPositionRandomizer.cs
@@ -5,10 +5,11 @@ using Random = UnityEngine.Random;
 public class CardPositionRandomizer : MonoBehaviour{
 	[SerializeField] private GameObject[] cards;
 	private void Awake(){
-		foreach (var card in cards){
+		foreach (var unused in cards){
 			var firstSelection = Random.Range(0, 5);
 			var secondSelection = Random.Range(6, 12);
-			(cards[firstSelection].transform.position, cards[secondSelection].transform.position) = (cards[secondSelection].transform.position, cards[firstSelection].transform.position);
+			(cards[firstSelection].transform.position, cards[secondSelection].transform.position) =
+				(cards[secondSelection].transform.position, cards[firstSelection].transform.position);
 		}
 	}
 }
