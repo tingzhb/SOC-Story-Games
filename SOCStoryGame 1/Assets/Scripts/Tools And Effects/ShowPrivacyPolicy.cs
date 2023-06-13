f44ac1850b6c6f959b20cbc9cc47da9dbf402742
tingzhb <tingzhb@gmail.com>
2023-06-13 14:00:17 +0200

    Reimpliment Privacy Policy Link

---
+++ SOCStoryGame 1/Assets/Scripts/Tools And Effects/ShowPrivacyPolicy.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class ShowPrivacyPolicy : MonoBehaviour{
+
+	public void Link(){
+		Application.OpenURL("https://github.com/tingzhb/DiscoveryPlay/blob/main/DiscoveryPlayPrivacyPolicy.md");
+	}
+}
