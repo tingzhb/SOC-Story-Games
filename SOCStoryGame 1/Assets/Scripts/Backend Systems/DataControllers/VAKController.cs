a699eef5957f20887d9f4cbf57ec369d434e1f53
tingzhb <tingzhb@gmail.com>
2023-09-04 17:01:11 +0200

    Implement VAK Monitor

--- SOCStoryGame 1/Assets/Scripts/Backend Systems/DataControllers/VAKController.cs
+++ SOCStoryGame 1/Assets/Scripts/Backend Systems/DataControllers/VAKController.cs
@@ -1,9 +1,6 @@
-using System;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 public class VAKController : MonoBehaviour{
-   [FormerlySerializedAs("vakModel")]
    [SerializeField] VAKModel playerVAKModel;
 
    void OnEnable(){
@@ -18,14 +15,5 @@ public class VAKController : MonoBehaviour{
       playerVAKModel.VValue += obj.V;
       playerVAKModel.AValue += obj.A;
       playerVAKModel.KValue += obj.K;
-      
-      Debug.Log("V Delta: " + obj.V);
-      Debug.Log("V: " + playerVAKModel.VValue);
-      
-      Debug.Log("A Delta: " + obj.A);
-      Debug.Log("A: " + playerVAKModel.AValue);
-
-      Debug.Log("K Delta: " + obj.K);
-      Debug.Log("K: " + playerVAKModel.KValue);
    }
 }
